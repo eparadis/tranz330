fad034b8fcca4d506aa87b3bc1448d3ae9fc3685
Ed P <eparadis@users.noreply.github.com>
2020-08-15 06:24:50 +0100

    pass compilation with sdcc

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ __sfr __at 0x23 DART_CTRL_B;
 
 __sfr __at 0x30 RTC_BASE; // RTC is 0x30 to 0x3F
 
-setup_PIO() {
+void setup_PIO() {
     // setup PIO port A
     PIO_CTRL_A=0xCF; // control each port bit individually
     PIO_CTRL_A=0x80; // bit 7 is input, the rest outputs
@@ -35,19 +35,9 @@ setup_PIO() {
     PIO_PORT_B=0x00; // initial outputs all low (TODO check this!!!)
 }
 
-setup_display() {
-    // TODO 
-    // "MSB of 1 is control byte, MSB of 0 is data byte"
-    byte current = PIO_PORT_A;
-    PIO_PORT_A = 0xEF & current; // clear bit 4 (display reset)
-    // TODO delay 0x1C cycles
-    PIO_PORT_A = 0x10 | current; // set bit 4 (display reset)
-    display_send_byte(0xFF); // set display duty cycle to 31 (max brightness)
-}
-
-display_send_byte(byte x) {
+void display_send_byte(char x) {
     // shift out byte on bit 5, MSB first, toggling clock (bit 6)
-    byte c = 0;
+    char c = 0;
     for( c=0; c<8; c++) {
         if( x & 0x80) {
             // TODO set data bit
@@ -59,13 +49,39 @@ display_send_byte(byte x) {
     }
 }
 
-display_send_byte_asm(byte x) {
+void setup_display() {
+    // TODO 
+    // "MSB of 1 is control byte, MSB of 0 is data byte"
+    char current = PIO_PORT_A;
+    PIO_PORT_A = 0xEF & current; // clear bit 4 (display reset)
+    // TODO delay 0x1C cycles
+    PIO_PORT_A = 0x10 | current; // set bit 4 (display reset)
+    display_send_byte(0xFF); // set display duty cycle to 31 (max brightness)
+}
+
+void setup_display_asm() {
+    __asm
+    in a, (#0)  ; get current port state
+    and #0xEF   ; clear bit 4 (display reset)
+    out (#0), A
+    ld b, #0x1c ; wait 0x1c cycles
+00001$:
+    djnz 00001$
+    or #0x10    ; set bit 4 (display reset)
+    out (#0), a
+    __endasm;
+
+    // set the display duty cycle to 31 (max brightness)
+    display_send_byte(0xff);
+}
+
+void display_send_byte_asm(char x) {
     // sdcc ASM notes:
     // see - https://gist.github.com/Konamiman/af5645b9998c802753023cf1be8a2970
     // Decimal numeric constants must be preceded with #
     // Hexadecimal numeric constants must be preceded with #0x
     // The syntax for offsets when using index registers is n(ix), where in other assemblers it's usually (ix+n)
-
+    x; // nop to silence compiler usage complaint
     __asm
     ld iy, #2
     add iy, sp  ; bypass the return address of the function in the stack
@@ -99,21 +115,22 @@ void display(char *msg) {
     }
 }
 
-#define NO_KEY -1
-void get_keypress() {
+#define NO_KEY 0xFF
+char get_keypress() {
     // TODO scan the keyboard once and return 0x0 to 0xF
     return NO_KEY;
 }
 
 char *itoa( int val) {
     // TODO do the modulo thing to convert to a string
+    val; // silence compiler for now
     return "0";
 }
 
-main() {
+void main() {
     // setup keypad and display
     setup_PIO();
-    setup_display();
+    setup_display_asm();
     // display hello world
     display("HELLO WORLD");
     
@@ -122,14 +139,9 @@ main() {
         c += 1;
         // wait for keypress
         while(get_keypress() == NO_KEY) {
-         __asm__("nop");
+            __asm__("nop");
         }
         // display a number
         display(itoa(c));
-    } while(true);
-
-    return 0;
+    } while(1);
 }
-
-
-
